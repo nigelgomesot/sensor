10343684afb7899b05d1d8769c6734fcec4dc577
Nigel Gomes <nigelgomes.ot@gmail.com>
2019-09-17 09:13:23 -0500

    # minor indentation fix...

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,11 +1,11 @@
 credentials = Aws::Credentials.new(
-  ENV['AWS_ACCESS_KEY_ID'], 
-  ENV['AWS_SECRET_ACCESS_KEY']
+  ENV['AWS_ACCESS_KEY_ID'], 
+  ENV['AWS_SECRET_ACCESS_KEY']
 )
 
 options = {
-  region: 'us-west-2',
-  credentials: credentials
+  region: 'us-west-2',
+  credentials: credentials
 }
 
 Aws.config.update(options)
