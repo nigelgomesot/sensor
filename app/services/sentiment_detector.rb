a0dbe577125a38ad1d50569c9aca28b34705181e
Nigel Gomes <nigelgomes.ot@gmail.com>
2019-10-02 09:48:55 -0500

    # refactor changes...

--- app/services/sentiment_detector.rb
+++ app/services/sentiment_detector.rb
@@ -1,78 +1,84 @@
 class SentimentDetector
-  def initialize(from_datetime: nil, upto_datetime: nil)
-    @from_datetime = from_datetime || Time.current.beginning_of_day
-    @upto_datetime = upto_datetime || Time.current
-    @messages = set_messages
-    @aws_client = set_aws_client
+  attr_reader :messages
+
+  def initialize(messages)
+    @messages = messages
+    @sentiments = []
+    set_aws_client
   end
 
   def execute!
-    get_sentiments!
+    detect_sentiments!
+    create_sentiments
   end
 
   private
 
-    def get_sentiments!
-      @messages.find_in_batches(batch_size: 25) do |message_batch|
-        # TODO: fetch sentiments and map to messages in the `batch`
-        message_array = message_batch.pluck(:id, :text)
-        text_list = message_array.collect { |_, text| text }
+    def detect_sentiments!
+      if messages.empty? || messages.length > 25
+        error_message = 'only 25 messages allowed'
+        Rails.logger.fatal(error_message)
 
-        response = @aws_client.batch_detect_sentiment(text_list)
+        raise error_message
+      end
 
-        if response['error_list'].present?
-          response['error_list'].each do |error|
-            message = message_batch[(error['index'])]
-            message_id = message.id
-            code = error['error_code']
-            message = ['error_message']
-            Rails.logger.error("message_id: #{message_id} code: #{code}, message: #{message}")
-          end
+      text_list = messages.map(&:text)
+      response = @aws_client.batch_detect_sentiment(text_list)
 
-          raise "AWS error"
+      if response['error_list'].present?
+        response['error_list'].each do |error|
+          message = @messages[(error['index'])]
+          message_id = message.id
+          code = error['error_code']
+          message = ['error_message']
+          error_message = "message_id: #{message_id} code: #{code}, message: #{message}"
+          Rails.logger.fatal(error_message)
         end
 
-        sentiments = response['result_list']
-        if sentiments && sentiments.empty?
-          message = "result is empty"
-          Rails.logger.error(message)
-          raise message
-        end
-        create_sentiments!(sentiments, message_batch)
+        raise "AWS error"
+      end
+
+      @sentiments = response['result_list']
+      if @sentiments && @sentiments.empty?
+        message = "result is empty"
+        Rails.logger.fatal(message)
+
+        raise message
       end
     end
 
-    def create_sentiments!(sentiments, message_batch)
-      sentiments.each do |sentiment|
-        message = message_batch[(sentiment['index'])]
-        sentiment_details = sentiment
+    def create_sentiments
+      @sentiments.each do |sentiment_detail|
+        begin
+          message = @messages[(sentiment_detail['index'])]
+          create_sentiment!(sentiment_detail, message)
+        rescue => err
+          error_message = "create sentiment error: message##{message.id}, #{err.message}"
 
-        Sentiment.create! do |s|
-          s.message = message
-          s.user = message.user
-          s.level = sentiment_details['sentiment'].downcase
-          s.mixed_score = sentiment_details['sentiment_score']['mixed']
-          s.negative_score = sentiment_details['sentiment_score']['negative']
-          s.neutral_score = sentiment_details['sentiment_score']['neutral']
-          s.positive_score = sentiment_details['sentiment_score']['positive']
+          Rails.logger.error(error_message)
         end
       end
     end
 
-    def set_messages
-      # TODO: fetch where sentiment is nil
-      @messages = Message.all
-        .where('created_at >= ?', @from_datetime)
-        .where('created_at <= ?', @upto_datetime)
+    def create_sentiment!(sentiment_detail, message)
+      Sentiment.create! do |s|
+        s.message = message
+        s.user = message.user
+        s.level = sentiment_detail['sentiment'].downcase
+        s.mixed_score = sentiment_detail['sentiment_score']['mixed']
+        s.negative_score = sentiment_detail['sentiment_score']['negative']
+        s.neutral_score = sentiment_detail['sentiment_score']['neutral']
+        s.positive_score = sentiment_detail['sentiment_score']['positive']
+      end
     end
 
     def set_aws_client
       comprehend_client = Aws::Comprehend::Client.new
-      Clients::Aws.new(comprehend_client)
+      @aws_client = Clients::Aws.new(comprehend_client)
     end
 end
 
 __END__
 
-sd = SentimentDetector.new(from_datetime: Time.current - 1.year)
+sd = SentimentDetector.new(Message.all)
 sd.execute!
