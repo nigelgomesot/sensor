c1a0e6f4e0263d4f4fccbc16afa8298f4d1600e6
Nigel Gomes <nigelgomes.ot@gmail.com>
2019-10-05 10:14:28 -0500

    # specs refactored...

--- spec/services/sentiment_detector_spec.rb
+++ spec/services/sentiment_detector_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe SentimentDetector, type: :service do
+  let(:message) { FactoryBot.create(:message) }
 
   describe '#initialize' do
-    let(:messages)  do
-      []
-    end
+    let(:messages) { [] }
+
     it 'assigns attributes' do
       detector = SentimentDetector.new(messages)
 
@@ -17,22 +17,54 @@ RSpec.describe SentimentDetector, type: :service do
 
   describe '#execute!' do
     let(:detector) { SentimentDetector.new(messages) }
-    let(:messages) { [] }
 
-    it 'raises error is messages is empty' do
-      expect do
-        detector.execute!
-      end.to raise_error(RuntimeError, /number of messages expected:/)
+    context 'with messages length' do
+      context 'when messages are empty' do
+        let(:messages) { [] }
+
+        it 'raises an error' do
+          expect do
+            detector.execute!
+          end.to raise_error(RuntimeError, /number of messages expected:/)
+        end
+      end
+
+      context 'when messages length > MESSAGES_MAX_LENGTH' do
+        let(:messages) { [message] }
+
+        it 'raises an error' do
+          stub_const("#{SentimentDetector}::MESSAGES_MAX_LENGTH", 0)
+
+          expect do
+            detector.execute!
+          end.to raise_error(RuntimeError, /number of messages expected/)
+        end
+      end
     end
 
-    it 'raises error is messages length > MESSAGES_MAX_LENGTH' do
-      stub_const("#{SentimentDetector}::MESSAGES_MAX_LENGTH", 0)
+    context 'AWS responses' do
+      let(:messages) { [message] }
+      let(:service_stub) do
+        Aws::Comprehend::Client.new(stub_responses: true)
+      end
+      let(:aws_client_stub) { Clients::Aws.new(service_stub) }
 
-      messages = [1]
+      before(:each) do
+        expect(detector).to receive(:aws_client)
+          .and_return(aws_client_stub)
+      end
 
-      expect do
-        detector.execute!
-      end.to raise_error(RuntimeError, /number of messages expected/)
+      it 'raises an error for AWS error response' do
+        error = StandardError.new('AWS SDK error')
+        service_stub.stub_responses(
+          :batch_detect_sentiment,
+          error,
+        )
+
+        expect do
+          detector.execute!
+        end.to raise_error(RuntimeError, /AWS SDK error/)
+      end
     end
   end
 end
